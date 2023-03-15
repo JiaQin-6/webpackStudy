e5f75ad2b642e78282fe5b38d862aa80a25caf80
David-at-547429205422 <1723470065@qq.com>
2023-03-15 13:25:07 +0800

    20230312

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-alert('1')
+import {createApp} from 'vue';
+import App from './App.vue';
+const app = createApp(App);
+app.mount('#app')
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,33 @@
-const path = require("path");
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry:'./src/index.js',
     output:{
-        path:path.join(__dirname,'/dist'),
         filename:'[name].js',
+        path:path.join(__dirname,'dist')
     },
+    mode:'production',
+    module:{
+        rules:[
+            {
+                test:/.js$/,
+                use:'babel-loader'
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+			title:'title',
+			template:'./index.html', //指定模板页面 
+			filename: 'index.html', // 输出文件【注意：这里的根路径是module.exports.output.path
+			showErrors: true,
+			//true|false，默认true；是否将错误信息输出到html页面中。这个很有用，在生成html文件的过程中有错误信息，输出到页面就能看到错误相关信息便于调试。
+			inject: 'body',
+			/*向template或者templateContent中注入所有静态资源，不同的配置值注入的位置不经相同
+			1、true或者body：所有JavaScript资源插入到body元素的底部
+			2、head: 所有JavaScript资源插入到head元素中
+			3、false： 所有静态资源css和JavaScript都不会注入到模板文件中*/
+			chunks: ["common",'index']
+		})
+    ]
 }
